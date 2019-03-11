8bf67b981214bd1915bf5d5e4dc825a2fb352124
Shawn Chen <cym@aibol.com>
2019-03-11 11:30:16 +0800

    add try/catch as service starting

--- server/Socket.Server/SocketServerService.cs
+++ server/Socket.Server/SocketServerService.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Net;
 using System.ServiceProcess;
 using log4net;
@@ -20,16 +21,23 @@ namespace Socket.Server
         {
             _logger.Info("service is going to start");
 
-            var maxConnections = int.Parse(ConfigurationManager.AppSettings["MaxConnections"]);
-            var listenIp = ConfigurationManager.AppSettings["ListenIP"];
-            var listenPort = int.Parse(ConfigurationManager.AppSettings["ListenPort"]);
-            var bufferSize = int.Parse(ConfigurationManager.AppSettings["BufferSize"]);
+            try
+            {
+                var maxConnections = int.Parse(ConfigurationManager.AppSettings["MaxConnections"]);
+                var listenIp = ConfigurationManager.AppSettings["ListenIP"];
+                var listenPort = int.Parse(ConfigurationManager.AppSettings["ListenPort"]);
+                var bufferSize = int.Parse(ConfigurationManager.AppSettings["BufferSize"]);
 
-            _server = new OTAServer(maxConnections, bufferSize);
-            _server.Initialize();
-            _server.Start(new IPEndPoint(IPAddress.Parse(listenIp), listenPort));
+                _server = new OTAServer(maxConnections, bufferSize);
+                _server.Initialize();
+                _server.Start(new IPEndPoint(IPAddress.Parse(listenIp), listenPort));
 
-            _logger.Info($"service started, listening on {listenIp}:{listenPort}");
+                _logger.Info($"service started, listening on {listenIp}:{listenPort}");
+            }
+            catch (Exception e)
+            {
+                _logger.Error($"service started error:{e.Message}");
+            }
         }
 
         protected override void OnStop()
