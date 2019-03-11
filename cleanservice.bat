35c33e582a8218d1962a88e573fa58bb50e4a980
Shawn Chen <cym@aibol.com>
2019-03-11 10:25:41 +0800

    upgrade clean service batch

--- cleanservice.bat
+++ cleanservice.bat
@@ -1,6 +1,16 @@
-sc query |find /i "SocketServer" >nul 2>nul 
-if not errorlevel 1 (goto exist)
+@echo off
+sc query |find /i "SocketServer" >nul 2>nul
+if not errorlevel 1 (goto exist) else goto notexist
 
 :exist
 sc stop SocketServer
 sc delete SocketServer
+
+goto :end
+
+:notexist
+echo not exist SocketServer
+
+goto :end
+
+:end
